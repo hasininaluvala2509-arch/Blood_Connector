72dd9a08532316fb04188297eecb193a663ea97f
Hasini Naluvala <hasininaluvala2509@gmail.com>
2026-06-11 18:19:04 +0530

    updated1

--- backend/server.js
+++ backend/server.js
@@ -47,7 +47,7 @@ const __dirname = path.dirname(__filename);
 if (process.env.NODE_ENV === "production") {
   const distPath = path.join(__dirname, "..", "frontend", "dist");
   app.use(express.static(distPath));
-  app.get("/*", (req, res) => res.sendFile(path.join(distPath, "index.html")));
+  app.get(/.*/, (req, res) => res.sendFile(path.join(distPath, "index.html")));
 }
 
 app.listen(PORT, () => {
